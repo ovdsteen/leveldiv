f3a279148ccedb01dc1eb2383618ccb358675e93
ovdsteen <ovdsteen@gmail.com>
2015-05-28 14:39:35 +0200

    change the way leveldiv init and added refresh function

--- index.js
+++ index.js
@@ -3,58 +3,66 @@
 var debounce = require('debounce'),
 	options = {};
 
-var level = function(){
+var leveldiv = {
 
-	if (!options.row) options.row = '[data-level-row]';
-	if (!options.column) options.column = '[data-level-column]';
+	set: function(opt){
 
-	if (options.debug ) console.log('options',options);
+		for (var prop in opt) {
+			options[prop] = opt[prop];
+		};
 
-	var rows, cols, i, c, l, a=[];
-
-	rows = document.querySelectorAll(options.row);
-
-	for (i = 0; i < rows.length; ++i) {
-
-		a = [];
-		cols = rows[i].querySelectorAll(options.column);
-
-		for (c = 0; c < cols.length; ++c) {
-			if ( options.responsive && document.body.clientWidth > options.responsive){
-				cols[c].style.height = '';
-				a.push(cols[c].offsetHeight)
+		document.onreadystatechange = function () {
+			if (document.readyState === "complete") {
+				leveldiv.level();
+				if (options.debounce){
+					window.onresize = debounce(leveldiv.level, options.debounce);
+				} else {
+					window.addEventListener("resize", leveldiv.level , true);
+				}
 			}
 		}
 
-		l = Math.max.apply(Math, a);
+	},
 
-		if (options.debug && a.length ) console.log('row '+(i+1)+' highest column is',l );
+	level: function(){
 
-		for (c = 0; c < cols.length; ++c) {
-			if ( options.responsive && document.body.clientWidth > options.responsive){
-				cols[c].style.height = l+'px';
-			}else{
-				cols[c].style.height = '';
+		if (!options.row) options.row = '[data-level-row]';
+		if (!options.column) options.column = '[data-level-column]';
+
+		if (options.debug ) console.log('options',options);
+
+		var rows, cols, i, c, l, a=[];
+
+		rows = document.querySelectorAll(options.row);
+
+		for (i = 0; i < rows.length; ++i) {
+
+			a = [];
+			cols = rows[i].querySelectorAll(options.column);
+
+			for (c = 0; c < cols.length; ++c) {
+				if ( options.responsive && document.body.clientWidth > options.responsive){
+					cols[c].style.height = '';
+					a.push(cols[c].offsetHeight)
+				}
+			}
+
+			l = Math.max.apply(Math, a);
+
+			if (options.debug && a.length ) console.log('row '+(i+1)+' highest column is',l );
+
+			for (c = 0; c < cols.length; ++c) {
+				if ( options.responsive && document.body.clientWidth > options.responsive){
+					cols[c].style.height = l+'px';
+				}else{
+					cols[c].style.height = '';
+				}
 			}
 		}
-	}
-}
+	},
 
-var leveldiv = function(opt){
-
-	for (var prop in opt) {
-		options[prop] = opt[prop];
-	};
-
-	document.onreadystatechange = function () {
-		if (document.readyState === "complete") {
-			level();
-			if (options.debounce){
-				window.onresize = debounce(level, options.debounce);
-			} else {
-				window.addEventListener("resize", level , true);
-			}
-		}
+	refresh: function(){
+		leveldiv.level();
 	}
 
 }
