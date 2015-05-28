45533d4add317b27d6de5c26d90c24aadc4846c1
ovdsteen <ovdsteen@gmail.com>
2015-05-28 14:38:27 +0200

    added refresh test functionality to example

--- example/src/script/index.js
+++ example/src/script/index.js
@@ -1,9 +1,24 @@
 var leveldiv = require('leveldiv');
 
-leveldiv( {
+leveldiv.set( {
 	debounce: 300,
 	responsive: 500,
 	debug: true,
 	row: '[data-level-row]',
 	column: '[data-level-column]'
 });
+
+document.getElementById("refresh").addEventListener("click", function( event ) {
+
+	event.preventDefault();
+
+	var cols = document.querySelectorAll('[data-level-column]');
+
+	for (c = 0; c < cols.length; ++c) {
+		cols[c].style.height = '';
+	}
+	setTimeout(function(){
+		leveldiv.refresh();
+	}, 500);
+
+}, false);
